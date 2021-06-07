964fa2d3326de5cdbcd13cc0a241fe8791bcc337
sawyer <cx@radxa.com>
2021-06-07 19:54:27 +0800

    rock-fuhai change release directory

--- RockPi4C/build-rockpi-fuhai-evb.sh
+++ RockPi4C/build-rockpi-fuhai-evb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 source device/rockchip/common/scripts/build-common.sh
-build-env       ROCKPI-4C
+build-env       ROCK-Fuhai
 build-uboot     rockpi4c
 build-kernel64  rockpi_4c rk3399-fuhai-evb
 build-aosp      RockPi4C userdebug
