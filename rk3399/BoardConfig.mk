f65db3b51ca20d4596488409ff2a7ce0567a9d85
Burkov Denis <hitechshell@mail.ru>
2026-08-26 23:31:54 +0500

    create rk3399-userdebug target based on rk3399_Android12
    Change-Id: Id9eac46430fa5ac3fd5af42b293462e13855f6dc

---
+++ rk3399/BoardConfig.mk
@@ -0,0 +1,24 @@
+include device/rockchip/rk3399/rk3399_BoardConfig.mk
+
+BOARD_SENSOR_ST := true
+BOARD_SENSOR_COMPASS_AK8963-64 := true
+BOARD_SENSOR_MPU_PAD := false
+BOARD_COMPASS_SENSOR_SUPPORT := true
+BOARD_GYROSCOPE_SENSOR_SUPPORT := true
+CAMERA_SUPPORT_AUTOFOCUS:= false
+
+BOARD_CAMERA_SUPPORT := true
+BOARD_CAMERA_SUPPORT_EXT := true
+PRODUCT_KERNEL_DTS := rk3399-rock960-ab
+PRODUCT_KERNEL_CONFIG := rockchip_defconfig android-11.config
+PRODUCT_KERNEL_VERSION := 5.10
+
+# AB image definition
+BOARD_USES_AB_IMAGE := false
+BOARD_ROCKCHIP_VIRTUAL_AB_ENABLE := false
+BOARD_HAS_RK_4G_MODEM := false
+
+ifeq ($(strip $(BOARD_USES_AB_IMAGE)), true)
+    include device/rockchip/common/BoardConfig_AB.mk
+    TARGET_RECOVERY_FSTAB := device/rockchip/rk3399/rk3399/recovery.fstab_AB
+endif
